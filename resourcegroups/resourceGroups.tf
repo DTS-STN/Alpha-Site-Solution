2f6890bbd4278076b7f32c95d99fc6455f072ae5
Eric Dube <ericdube88@gmail.com>
2021-06-14 15:56:27 -0400

    Saving tfstate back to json, readded depot

--- resourcegroups/resourceGroups.tf
+++ resourcegroups/resourceGroups.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "appService" {
   name = var.appservice_resource_group
   location = var.location
 }
+
+resource "azurerm_resource_group" "depot" {
+  name = var.depot_resource_group
+  location = var.location
+}
