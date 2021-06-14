877caa3061ab9a3c7712cfc420c584817756d897
Eric Dube <ericdube88@gmail.com>
2021-06-14 16:35:27 -0400

    fixed issues

--- appservice/appService.tf
+++ appservice/appService.tf
@@ -47,7 +47,16 @@ resource "azurerm_app_service" "app-service-primary" {
     "SLOT_NAME"                       = "default"
     "MONGO_URL"                       = var.mongo_uri
     "MONGO_DB"                        = "researchpool"
-
+    "REPORT_A_PROBLEM_ENABLED"        = "true"
+    "USER_SIGNUP_ENABLED"             = "true"
+    "USER_SIGNUP_FRENCH_TEMPLATE_ID"  = var.USER_SIGNUP_FRENCH_TEMPLATE_ID
+    "USER_SIGNUP_ENGLISH_TEMPLATE_ID" = var.USER_SIGNUP_ENGLISH_TEMPLATE_ID
+    "NOTIFY_BASE_API_URL"             = var.NOTIFY_BASE_API_URL
+    "NOTIFY_API_KEY"                  = var.NOTIFY_API_KEY
+    "NOTIFY_REPORT_A_PROBLEM_EMAIL"   = var.NOTIFY_REPORT_A_PROBLEM_EMAIL
+    "NOTIFY_REPORT_A_PROBLEM_TEMPLATE_ID" = var.NOTIFY_REPORT_A_PROBLEM_TEMPLATE_ID
+    "MONGO_URL"                       = var.MONGO_URL
+    "MONGO_DB"                        = var.MONGO_DB
   }
 
 }
