e9d8721fd2ea0ab30d76cf858ee4807cc3fe366b
Eric Dube <ericdube88@gmail.com>
2021-07-06 13:15:02 -0400

    Removed unused comments

--- createContainer.sh
+++ createContainer.sh
@@ -14,16 +14,6 @@ echo $terraform_sp_name
 echo "Setting account"
 az account set --subscription $subscription_id
 
-echo "Creating Terraform Service Principal..."
-#TODO GET SP CREDS FROM KEYVAULT
-# export terraform_sp_pass=$(az ad sp create-for-rbac --name $terraform_sp_name --role Contributor | grep password | cut -c16-49)
-# export terraform_sp_id=$(az ad sp list --display-name $terraform_sp_name --query [].appId -o tsv)
-echo "Done."
-
-# echo "Setting Terraform SP ID and Pass in VarFile"
-# sed -i "s/%CLIENT_ID%/$terraform_sp_id/g" $VARFILE
-# sed -i "s/%CLIENT_SECRET%/$terraform_sp_pass/g" $VARFILE
-#echo "Done"
 echo $remote_state_storage_account_name
 echo "Creating remote tfstate container..."
 export AZURE_STORAGE_KEY=$(az storage account keys list -g $tfstate_storage_rg -n $remote_state_storage_account_name --query [0].value -o tsv)
