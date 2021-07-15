679dadef163b54e6f4f44c78b3072fec6afc549e
Eric Dube <ericdube88@gmail.com>
2021-07-15 15:00:37 -0400

    Added HTTP redirect

--- webapplicationgateway/diagnostics.tf
+++ webapplicationgateway/diagnostics.tf
@@ -1,5 +1,5 @@
 resource "azurerm_monitor_diagnostic_setting" "application-primary-diagnostics" {
-  name                = "${application}-ag-diagnostics-primary-${var.environment}"
+  name                = "${var.application_name}-ag-diagnostics-primary-${var.environment}"
   target_resource_id = azurerm_application_gateway.application-gateway-v2-primary.id
   storage_account_id = var.diagnostic_storage_account_id
 
