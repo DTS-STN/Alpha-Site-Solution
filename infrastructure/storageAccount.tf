bed3afc596e3d291dca4102c2487d720e46bd9fd
Eric Dube <ericdube88@gmail.com>
2021-07-20 11:11:48 -0400

    Added log analytics to wag

--- infrastructure/storageAccount.tf
+++ infrastructure/storageAccount.tf
@@ -1,10 +1,10 @@
 resource "random_string" "storageName" {
-  length           = 16
+  length           = 4
   special          = false
 }
 
 resource "azurerm_storage_account" "appservice-diagnostics" {
-  name                     = lower(random_string.storageName.result)
+  name                     = "${var.application_name}diag${var.environment}${lower(random_string.storageName.result)}"
   resource_group_name      = var.depot_resource_group
   location                 = var.location
   account_tier             = "Standard"
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "appservice-diagnostics" {
 }
 
 resource "azurerm_storage_account" "storageacct" {
-  name                     = "${var.application_name}sa{var.environment}"
+  name                     = "${var.application_name}sa${var.environment}"
   resource_group_name      = var.depot_resource_group
   location                 = var.location
   account_tier             = "Standard"
